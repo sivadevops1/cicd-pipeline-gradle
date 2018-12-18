0c91b64ee9dbf7b3fa5476b687fd772cdb9df646
sivadevops1 <45669583+sivadevops1@users.noreply.github.com>
2018-12-18 09:43:58 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -25,6 +25,7 @@ docker --version
 #docker image rm $(docker image ls -a -q)   # Remove all images from this machine
 #docker login             # Log in this CLI session using your Docker credentials
 #docker tag <image> username/repository:tag  # Tag <image> for upload to registry
+#docker login                                #To login to the dockerhub registry
 #docker push username/repository:tag            # Upload tagged image to registry
 #docker run username/repository:tag                   # Run image from a registry
 #docker exec -it c7d9bb3f72c3  bash   #get into the docker container.
@@ -32,3 +33,18 @@ docker --version
 
 #Best dockerfile explanation - https://takacsmark.com/dockerfile-tutorial-by-example-dockerfile-best-practices-2018/#dockerfile-basics
 
+#####
+#To Create the Docker images dynamically after lot of chnages inside.
+
+#Run the docker image with -i - interactive option
+sudo docker run -i -t ubuntu bash
+
+#Do all the things needed, commit the container to a new image
+# df - is the container id , ubuntu-apache is the new image where this container needs to be committed to
+sudo docker commit df888c72ed5a ubuntu-apache
+
+#check logs
+#Get the container id which stopped/problematic
+sudo docker container ps -a
+sudo docker logs <containerid>
+#NOTE:Sometimes your docker container exits with (0) and logs also doesn't show any problem Run the image in interactive mode
